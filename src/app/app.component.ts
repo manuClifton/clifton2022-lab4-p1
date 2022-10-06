c293413913818429fd2eb6ea934731cd88c618cb
manuClifton <manuclifton@gmail.com>
2022-10-06 11:36:49 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'clifton2022-lab4-p1';
+}
